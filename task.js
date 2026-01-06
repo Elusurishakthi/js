27bca386fd48ca2a1556f604cf7491bf3eaa3e74
Elusurishakthi <elusurishakthi@gmail.com>
2026-01-06 11:42:38 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,7 @@
+const name ="Shakthi";
+let age=20;
+ let isstudent=true;
+
+console.log(name,typeof((name)));
+console.log(age,typeof((age)));
+console.log(isstudent,typeof((isstudent)));                                                        
